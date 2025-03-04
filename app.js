9ef19fee0e57de3ef9737da6e018d971d4c6595a
eb94596 <50734136+eb94596@users.noreply.github.com>
2025-03-04 09:13:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
+  res.end('Hello world!-0.2');
 });
 
 server.listen(8080, () => {
