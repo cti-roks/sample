a94d98ec1b71741292c4b6c6c3b081f8f2598cac
Yongji-Li <192184280+Yongji-Li@users.noreply.github.com>
2025-03-05 16:08:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!-cicd');
+  res.end('Hello world!-0.2');
 });
 
 server.listen(8080, () => {
